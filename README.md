50de147dc37141697e450bcca0dc646dc2b88a6e
MateuszBuchner <mateusz.buchner369@gmail.com>
2022-11-04 19:39:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Karta Kredytowa
+
+Postawiłem małego forma, w którym dodaje kartę do inputa i dopisuje spacje. Przed zapisaniem danych w controlerze funkcja str_replace zwraca całego stringa bez spacji (przed: 19 znaków) po (16: znaków)
+
+- str_replace
+- https://jsfiddle.net/pmrotule/217u7fru/ code z stackoverflow
---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+
+class AccountsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('accounts.index', [
+            'accounts' => Account::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $account = new Account($request->all());
+        $account->card = str_replace(' ','',$account->card);
+        $account->save();
+        return redirect('accounts');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
